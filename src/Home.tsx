320d8891eb63edbef34249af9a441cf162363873
DeepGreenAI <william@deepgreenai.com>
2023-07-07 17:19:40 -0400

    update home

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,17 +13,14 @@ const Home = () => {
   const refreshPage = () => {
     window.location.reload();
   };
-
-  if (activePage === "home") {
-    return (
-      <div id="homepage">
-        <div id="centered-div">
-        <img id="num-img" src="Numb3r.png"/>
-        <img id="binary-num-img" src="binary-numbers.png"/>
-        </div>
+  return (
+    <div id="homepage">
+      <div id="centered-div">
+      <img id="num-img" src="Numb3r.png"/>
+      <img id="binary-num-img" src="binary-numbers.png"/>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Home;
